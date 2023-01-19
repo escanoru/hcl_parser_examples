769d46dc0a4301298c7662ab10a15903493c1955
escanoru <yitzoyi@outlook.com>
2023-01-19 15:10:57 +0000

    first commit

---
+++ hcl.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hashicorp/hcl/v2/hclsimple"
+)
+
+type Config struct {
+	LogLevel string `hcl:"log_level"`
+}
+
+func main() {
+	var config Config
+	err := hclsimple.DecodeFile("config.hcl", nil, &config)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %s", err)
+	}
+	log.Printf("Configuration is %#v", config)
+}
